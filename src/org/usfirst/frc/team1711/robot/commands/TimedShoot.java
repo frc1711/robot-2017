60cadac4423393d3c6430390fc8aeef7ea8e84c2
Raptor 1711 Yellow Laptop <raptors1711@gmail.com>
2017-02-13 18:56:01 -0500

    Added second shooter

--- src/org/usfirst/frc/team1711/robot/commands/TimedShoot.java
+++ src/org/usfirst/frc/team1711/robot/commands/TimedShoot.java
@@ -36,6 +36,6 @@ public class TimedShoot extends TimedCommand {
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
-    protected void interrupted() {
+    protected void interrupted() { 
     }
 }
