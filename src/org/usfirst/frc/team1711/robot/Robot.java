1f3239521825c1dc6b29949160f20b1bc3376737
Raptor 1711 Yellow Laptop <raptors1711@gmail.com>
2017-02-18 13:56:27 -0500

    Added encoder rate to smartdashboard

--- src/org/usfirst/frc/team1711/robot/Robot.java
+++ src/org/usfirst/frc/team1711/robot/Robot.java
@@ -48,7 +48,7 @@ public class Robot extends IterativeRobot {
 
 	/**
 	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
+	 * used for any initialization code. 00
 	 */
 	@Override 
 	public void robotInit() {
